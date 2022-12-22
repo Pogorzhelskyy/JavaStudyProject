6b9ee593c0aa54bccc1791f0e759e710492bc856
Pogorzhelskyy <mpogorzhelskyy@gmail.com>
2022-12-22 20:28:25 +0200

    Homework 8 (Loops). Selection menu via scanner and switch/case. Method "create" in Serv class. Obj stores in array and using 2-nd counter this reason. 2 ways of termination program (by the end line and System.exit(0)).

---
+++ OnlineSchoolPogorzhelskyy/src/hw8/models/HomeWork.java
@@ -0,0 +1,11 @@
+package hw8.models;
+
+public class HomeWork {
+    private int id;
+    public static int counter;
+
+    public HomeWork (int id) {
+        this.id = id;
+        counter ++;
+    }
+}
